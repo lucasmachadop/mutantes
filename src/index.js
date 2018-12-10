649a0638ee4c0631259307f9dbed9ae4e10fdd2d
lucas peres <lucas.peres@contabilizei.com.br>
2018-12-10 19:17:52 -0200

    configurando para primeiro deploy da function

--- src/index.js
+++ src/index.js
@@ -11,24 +11,10 @@ app.use(routes)
 
 const SERVER_PORT = 3001
 
-// if (isDevelopmentEnv()) {
-
 app.listen({ port: SERVER_PORT }, () => console.log(`LISTENING ON PORT ${SERVER_PORT}`))
 
-// }
-//
-// if (isProductionEnv()) {
-//
-//     sso.setAppCredentials(ssoCredentials)
-//     crawlerJucespRoutes.use(sso.verify)
-//
-//     exports.jucespCrawler = functions.https.onRequest((request, response) => {
-//             if (!request.path) {
-//         request.url = `/${request.url}` // prepend '/' no final da url para manter os query params
-//     }
-//     return app(request, response)
-// })
-//
-//     console.log('functions service started')
-//
-// }
+exports.isMutant = functions.https.onRequest((request, response) => {
+    if (!request.path) {
+        request.url = `/${request.url}` // prepend '/' no final da url para manter os query params
+    }
+})
